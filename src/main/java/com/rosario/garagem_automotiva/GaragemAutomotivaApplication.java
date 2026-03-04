5d6322230f9bdc77be0544d05eb1103df07157d2
MatheusDoRosario <matheusxdargws@gmail.com>
2026-03-04 09:32:51 -0300

    Initial commit

---
+++ src/main/java/com/rosario/garagem_automotiva/GaragemAutomotivaApplication.java
@@ -0,0 +1,13 @@
+package com.rosario.garagem_automotiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragemAutomotivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragemAutomotivaApplication.class, args);
+	}
+
+}
