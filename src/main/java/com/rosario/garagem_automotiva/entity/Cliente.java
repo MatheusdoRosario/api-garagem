01c1570cdcdccfa462661f89697e628fa58de1ad
MatheusDoRosario <matheusxdargws@gmail.com>
2026-03-04 09:39:48 -0300

    Adiciona o Cliente

---
+++ src/main/java/com/rosario/garagem_automotiva/entity/Cliente.java
@@ -0,0 +1,38 @@
+package com.rosario.garagem_automotiva.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String telefone;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+}
