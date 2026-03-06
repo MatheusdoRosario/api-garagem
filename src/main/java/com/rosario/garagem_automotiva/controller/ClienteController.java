83b13e36629c38dcbaaf492fa9ea7fe5cc819d38
MatheusDoRosario <matheusxdargws@gmail.com>
2026-03-06 10:36:52 -0300

    Implementa o ClienteController

---
+++ src/main/java/com/rosario/garagem_automotiva/controller/ClienteController.java
@@ -0,0 +1,52 @@
+package com.rosario.garagem_automotiva.controller;
+
+import com.rosario.garagem_automotiva.dto.CadastroClienteDTO;
+import com.rosario.garagem_automotiva.dto.ClienteDTO;
+import com.rosario.garagem_automotiva.exception.ValidacaoException;
+import com.rosario.garagem_automotiva.service.ClienteService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService service;
+
+    @GetMapping("/nome/{nome}")
+    public ResponseEntity<Page<ClienteDTO>> listarClientePorNome(@PathVariable String nome, @RequestBody Pageable pageable) {
+        Page<ClienteDTO> clientes = service.listarClientesPorNome(nome, pageable);
+        return ResponseEntity.ok(clientes);
+    }
+
+    @GetMapping("/telefone/{telefone}")
+    public ResponseEntity<Page<ClienteDTO>> listarClientePorTelefone(@PathVariable String telefone, @RequestBody Pageable pageable) {
+        Page<ClienteDTO> clientes = service.listarClientesPorTelefone(telefone, pageable);
+        return ResponseEntity.ok(clientes);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> cadastrarCliente(@RequestBody @Valid CadastroClienteDTO dto) {
+        try {
+            service.cadastrarCliente(dto);
+            return ResponseEntity.ok().body("Cliente cadastrado com sucesso!");
+        } catch (ValidacaoException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<String> atualizarCliente(@RequestBody @Valid ClienteDTO dto) {
+        try {
+            service.atualizarCliente(dto);
+            return ResponseEntity.ok().body("Cliente atualizado com sucesso!");
+        } catch (ValidacaoException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
